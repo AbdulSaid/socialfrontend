2b442d6199ff2dfe87e9e3baf664a264a8da7386
Abdul Said <abdulrahman.asaid@gmail.com>
2022-02-04 16:12:20 -0500

    Created App State hook, all application can use state

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AppState } from "./AppState";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AppState>
+      <Router>
+        <App />
+      </Router>
+    </AppState>
   </React.StrictMode>,
   document.getElementById("root")
 );
