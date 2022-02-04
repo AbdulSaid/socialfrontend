dcc81c3926d57edcd074509f973b43d681c64530
Abdul Said <abdulrahman.asaid@gmail.com>
2022-02-04 16:40:14 -0500

    Add links to Nav and style

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-    <div>
-      <h1>Navigation</h1>
-    </div>
+    <header>
+      <h1>Fake Social Media</h1>
+      <nav>
+        <Link to="/">
+          <div>Home</div>
+        </Link>
+        <Link to="/auth/signup">
+          <div>Signup</div>
+        </Link>
+        <Link to="/auth/login">
+          <div>Login</div>
+        </Link>
+      </nav>
+    </header>
   );
 }
 
