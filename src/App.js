fb7454ea688ec78550fecdbc8de9583f9ebe21a1
Abdul Said <abdulrahman.asaid@gmail.com>
2022-02-04 16:30:40 -0500

    Add all basic components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./Components/Navigation";
+import Home from "./Pages/Home";
+import Auth from "./Pages/Auth";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route path="/auth/:form" component={Auth} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Routes>
+    </>
   );
 }
 
